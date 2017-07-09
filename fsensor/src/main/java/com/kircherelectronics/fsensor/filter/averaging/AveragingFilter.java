484a7a0c01ce67c984ca720b2e38bbdf05606d02
KalebKE <kaleb.kircher@gmail.com>
2017-07-09 09:57:48 -0600

    Added comments and licences.

--- fsensor/src/main/java/com/kircherelectronics/fsensor/filter/averaging/AveragingFilter.java
+++ fsensor/src/main/java/com/kircherelectronics/fsensor/filter/averaging/AveragingFilter.java
@@ -2,7 +2,25 @@ package com.kircherelectronics.fsensor.filter.averaging;
 
 import com.kircherelectronics.fsensor.filter.BaseFilter;
 
+/*
+ * Copyright 2017, Kircher Electronics, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
+ * A base implementation of an averaging filter.
+ *
  * Created by kaleb on 7/6/17.
  */
 
